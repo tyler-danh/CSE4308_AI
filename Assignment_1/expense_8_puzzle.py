38f9f9c94421abeaf907d3d4af2cb313044443f6
tyler-danh <122916035+tyler-danh@users.noreply.github.com>
2024-09-02 18:27:29 -0500

    progress on BFS and trace file gen

--- Assignment_1/expense_8_puzzle.py
+++ Assignment_1/expense_8_puzzle.py
@@ -1,21 +1,67 @@
 import sys
 import numpy as np
+from collections import deque
+import datetime
 
 def main(method): #here using method to decide which search method to invoke
-    if method == "bfs":
+    if method == "bfs":         #breadth first search
         bfs(start, goal, dflag)
-    elif method == "ucs":
+    elif method == "ucs":       #uniform cost
         ucs(start, goal, dflag)
-    elif method == "greedy":
+    elif method == "greedy":    #greedy search
         greedy(start, goal, dflag)
-    elif method == "a*":
+    elif method == "a*":        #A* search
         a_star(start, goal, dflag)
     else:
         a_star(start, goal, dflag)
 
 
-def bfs(start, goal, flag):
+def bfs(start, goal, flag): #bfs's fringe is a FIFO so i should try and implement a queue
     print("this is bfs")
+    #for BFS i will start at whichever node is 0
+    #cant use a for loop to search through array because i am using numpy
+    nodes = []
+    for i in range(9):
+        index = np.where(start == i)
+        for i, j in zip(index[0], index[1]):
+            node = (i,j)
+            nodes.append(node)
+    for node in nodes: #finding each node's neighbors
+        i,j = node
+        print(f"neighbors of {start[node]}")
+        if (i == 2 and j == 2) or (i == 2 and j > 0) or (j == 2 and i > 0):
+            print(f"{start[i-1, j]}, ({i-1}, {j})")
+            print(f"{start[i, j-1]}, ({i}, {j-1})")
+            continue
+        elif (i == 0 and j == 0) or (i == 0 and j < 2) or (j == 0 and i < 2):
+            print(f"{start[i+1, j]}, ({i+1}, {j})")
+            print(f"{start[i, j+1]}, ({i}, {j+1})")
+            continue
+        elif i == 1 and j == 1:
+            print(f"{start[i-1, j]}, ({i-1}, {j})")
+            print(f"{start[i, j-1]}, ({i}, {j-1})")
+            print(f"{start[i+1, j]}, ({i+1}, {j})")
+            print(f"{start[i, j+1]}, ({i}, {j+1})")
+        elif i == 2 and j == 0:
+            print(f"{start[i-1, j]}, ({i-1}, {j})")
+            print(f"{start[i, j+1]}, ({i}, {j+1})")
+        elif i == 0 and j == 2:
+            print(f"{start[i+1, j]}, ({i+1}, {j})")
+            print(f"{start[i, j-1]}, ({i}, {j-1})")
+        
+    """queue = deque([start_node])
+    visited = set([start_node]) #i do not want repeated states
+    print(queue)
+    print(visited)
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.add(node)
+            print(visited)
+            for adj in start[node]:
+                if adj not in visited:
+                    queue.append(adj)"""
+
     if np.array_equal(start, goal):
         return 0
     
@@ -54,17 +100,25 @@ if __name__ == "__main__":
             goal_file = sys.argv[2]
             method = "a*"
             dflag = False
-        
         with open(start_file, 'r') as file:
             start = file.readlines()
             start = [start.split() for start in start]  #splitting start contents by each character instead of each line
         with open(goal_file, 'r') as file:
             goal = file.readlines()
             goal = [goal.split() for goal in goal]     #splitting goal contents by each character instead of each line
+        if dflag == "True":
+            print("dflag true")
+            cdatetime = datetime.datetime.now()
+            dfilename = cdatetime.strftime("trace-%m_%d_%Y-%H_%M") + ".txt" 
+            with open(dfilename, "w") as file:
+                print("HELLO WORLD!", file=file)
         
+        #making sure each element in the list is an int
         #turning the start & goal lists into matrices
-        start = np.matrix(start)
-        goal = np.matrix(goal)
+        start = np.vectorize(int)(start)
+        start = np.array(start)
+        goal = np.vectorize(int)(start)
+        goal = np.array(goal)
 
         main(method)
     
