532a007c795ef5c14727039a6e9f840f2b4c1551
tyler-danh <122916035+tyler-danh@users.noreply.github.com>
2024-09-04 22:31:30 -0500

    created class node

---
+++ Assignment_1/Node.py
@@ -0,0 +1,11 @@
+class node:
+    def __init__(this, state, parent, cost, heuristic = 0):
+        this.state = state          #some matrix, successor or start
+        this.parent = parent        #prev node. if none then parent = 0
+        this.cost = cost            # will be the value of the matrix element
+        this.heuristic = heuristic #will be used in greedy/A*, default is 0
+
+        def __eq__(this, other):
+            return this.state == other.state
+        def __lt__(this, other):
+            return this.cost + this.heuristic < other.cost + other.heuristic #adding heuristic should not matter in bfs or ucs
